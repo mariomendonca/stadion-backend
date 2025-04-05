2dda17796cf573a0b7139713f6b228f9614fa596
mariomendonca <mariomeendonca00@gmail.com>
2025-04-05 15:49:30 -0300

    change cors

--- src/main/java/com/stadion/stadion_backend/config/SecurityConfig.java
+++ src/main/java/com/stadion/stadion_backend/config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedOrigins(List.of("https://stadion-web.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://stadion-web.onrender.com", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
